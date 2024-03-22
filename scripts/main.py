f19d7f15a4582fc68bc224b8c593b746df3eec00
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-22 23:16:36 +0100

    added sample scripts

---
+++ scripts/main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from pathlib import Path
+path_root = Path(__file__).parents[1]
+sys.path.append(os.path.join(path_root))
+
+from jsonservice import JsonService
+
+users_path = os.path.join(path_root, 'data', 'tests.json')
+
+if __name__ == "__main__":
+    service = JsonService(users_path)
+    service.write('settings.lighthouse', 'on')
+
+
+
+
