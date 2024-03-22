73f32f29a7d5024a0600374b6c029d404ff58ce0
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-22 23:43:55 +0100

    fixed path

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 path_root = Path(__file__).parents[1]
-sys.path.append(os.path.join(path_root))
+sys.path.append(os.path.join(path_root, 'src'))
 
 from jsonservice import JsonService
 
