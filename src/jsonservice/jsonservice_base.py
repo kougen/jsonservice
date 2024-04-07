c51aec35f0d14e2b1daf6075dad60dd94a6eb557
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-07 16:49:30 +0200

    added json service base

--- src/jsonservice/jsonservice_base.py
+++ src/jsonservice/jsonservice_base.py
@@ -1,72 +1,31 @@
-import os
-import json
-
 
 class JsonServiceBase:
-    def _read(self, key) -> dict | list | None:
-        if '.' in key:
-            return self._read_subkey(key.split('.'), self._data)
-        else:
-            return self._read_subkey([key], self._data)
-
-
-    def read_all(self) -> dict | list:
-        return self._data
-
-
-    def read_index(self, index: int) -> dict | list | None:
-        if type(self._data) is list:
-            if index < len(self._data):
-                return self._data[index]
-        return None
-
-
-    def _read_subkey(self, keys: list[str], source: dict):
-        if len(keys) == 0 or type(source) is not dict:
-            return None
-
-        if len(keys) == 1:
-
-            if keys[0] in source.keys():
-                return source[keys[0]]
-            else:
-                return None
-
-        if keys[0] in source.keys():
-            return self._read_subkey(keys[1:], source[keys[0]])
-
-        return None
-
-
-    def _write(self, path: str, value):
+    def read(self, key) -> dict | list | None:
         pass
 
+    def read_all(self) -> dict | list:
+        pass
 
-class JsonService:
-    def __init__(self, json_path: str, create_if_not_exists: bool = True, default_data: type | None = None):
+    def read_index(self, index: int) -> dict | list | None:
+        pass
 
+    def write(self, path: str, value):
+        """
+        Write a value to a dictionary or list.
+        :param path: The path to the value in the dictionary or list. Example: 'settings.lighthouse'
+        :param value: The value to write.
+        :return: None
+        """
+        pass
 
-        if not os.path.exists(json_path) and not create_if_not_exists:
-            print(f"The given json file does not exists! ({json_path})")
-            exit(1)
+    def write_sub_list(self, root_key: str, path: str, value):
+        """
+        Write a value to a list in a dictionary or list.
 
-        if default_data != {} and default_data != []:
-            print("Default data can only be an empty dictionary or list!")
-            exit(1)
-
-        if not os.path.exists(json_path):
-            with open(json_path, 'w') as outfile:
-                json.dump(default_data, outfile)
-
-        self._json_path = json_path
-        json_data = open(self._json_path, 'r').read()
-        self._data = json.loads(json_data)  # type:dict | list
-
-
-    def __setitem__(self, key, value):
-        self._write(key, value)
-
-    def __getitem__(self, item):
-        return self.read(item)
-
-
+        :param root_key: The key of the dictionary or list. If the root_key is a list, the index will be used. If the
+        root_key is a dictionary, the key will be used.
+        :param path: The path to the list in the dictionary or list.
+        :param value: The value to write.
+        :return: None
+        """
+        pass
